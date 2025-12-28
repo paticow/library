4c38d03cd2e09b668a3659d7ea4a3ef424308696
paticow <patimatanazareth@gmail.com>
2025-12-27 20:02:17 -0400

    Add functions and basic structure

--- books.js
+++ books.js
@@ -1,12 +1,31 @@
-function books(title, author, pages, read) {
+const myLibrary = [];
+
+function Book(title, author, pages, isRead) {
+  this.id = crypto.randomUUID(); 
   this.title = title;
   this.author = author;
   this.pages = pages;
-  this.read = read;
-
-  console.log(`${this.title}, ${this.author}, ${this.pages}, ${this.read}`);
+  this.isRead = isRead;
 }
 
-let theHobbit = new books("The hobbit", "J.R.R. Tolkien", "295", "not read"); // "The Hobbit by J.R.R. Tolkien, 295 pages, not read yet"
+function addBookToLibrary(title, author, pages, isRead) {
+  const newBook = new Book(title, author, pages, isRead);
+  myLibrary.push(newBook);
+}
 
+function libraryLoop(){
+  myLibrary.forEach(book => {
+  console.log(`Title: ${book.title}`);
+});
+}
 
+addBookToLibrary("el pepe", "ete sech", "69", "read");
+addBookToLibrary("eso tilin", "car", "120", "not read");
+addBookToLibrary("linkedin", "churro", "1454", "not read");
+
+/*
+document.getElementById("demo1").innerHTML =
+"book name:" + element.title + "autor" + element.age + "."; 
+*/
+
+libraryLoop();
